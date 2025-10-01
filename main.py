47bdfb42212d1f60aaf6835676897289e9a0a816
Camara7577 <camarasetedp@outlook.com>
2025-10-01 14:00:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.write('Colunas disponíveis:', df.columns.tolist())
-
-df = pd.read_csv('base_funcionarios.csv', delimiter=';')
+df = pd.read_csv(r'D:\Users\warleandro.reginaldo\Documents\DEPARTAMENTO PESSOAL - RH 2025\PROGRAMA\PROJETO STREAMLIT\base_funcionarios.csv', delimiter=';')
 
 st.title('Dashboard RH – Visualização de Perfil dos Servidores')
 
@@ -14,11 +12,11 @@ genero = st.multiselect('Gênero:', df['Gênero'].unique(), default=df['Gênero'
 df_filt = df[df['Faixa Idade'].isin(faixas_idade) & df['Gênero'].isin(genero)]
 
 st.subheader('Distribuição por Faixa Etária e Gênero')
-fig1 = px.bar(df_filt, x='Faixa Idade', color='Gênero', barmode='group')
+fig1 = px.bar(df_filt, x='Faixa Idade', color='Gênero')
 st.plotly_chart(fig1)
 
 st.subheader('Distribuição por Tempo de Casa')
-fig2 = px.bar(df_filt, x='Faixa Tempo de Casa', color='Gênero', barmode='group')
+fig2 = px.bar(df_filt, x='Faixa Tempo de Casa', color='Gênero')
 st.plotly_chart(fig2)
 
 st.subheader('Proporção de Gênero')
